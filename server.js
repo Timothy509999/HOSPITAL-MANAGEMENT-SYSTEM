d3c7bdc3fbcc1cb1a90b3ccdc306f0f97e3c089f
olajubu timothy <olajubutimothy8@gmail.com>
2025-05-07 15:33:11 +0100

    fix cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Enhanced CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: ['http://localhost:5173', 'https://hospital-management-system-frontend-three.vercel.app'], // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
